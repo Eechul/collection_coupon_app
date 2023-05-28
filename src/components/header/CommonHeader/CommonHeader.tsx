3a20eca5cf44b50cb345f14526769a6abeefd462
이동철 <choise154@gmail.com>
2023-05-28 10:41:23 +0900

    뒤로가기 버튼 추가

--- src/components/header/CommonHeader/CommonHeader.tsx
+++ src/components/header/CommonHeader/CommonHeader.tsx
@@ -1,10 +1,20 @@
+'use client'
+
+import BackIcon from '@/components/icon/BackIcon'
+import { useRouter } from 'next/navigation'
+
 interface CommonHeaderProps {
   title: string;
 }
 
 export default function CommonHeader({ title }: CommonHeaderProps) {
+  const router = useRouter()
 
   return (
-      <div className='text-center text-black h-16 border-b border-b-gray align-middle py-4'>{title}</div>
+      <div className='flex justify-between text-center text-black h-16 border-b border-b-gray align-middle p-4'>
+        <button onClick={() => router.back()}><BackIcon /></button>
+        <span>{title}</span>
+        <span></span>
+      </div>
   )
 }
