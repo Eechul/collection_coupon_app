44a1bbb39824b7de5018d73fa222628caeec23ca
이동철 <choise154@gmail.com>
2023-06-03 20:03:31 +0900

    궁금증 주석

--- src/components/pad/NumberDisplayPad/NumberDisplayPad.tsx
+++ src/components/pad/NumberDisplayPad/NumberDisplayPad.tsx
@@ -8,9 +8,15 @@ interface NumberDisplayPadProps {
   submitButtonText: string;
   movePage: string;
   randomPadNumber?: boolean;
+  action?: any;
 }
 
-export default function NumberDisplayPad({ submitButtonText, movePage, randomPadNumber = false }: NumberDisplayPadProps) {
+export default function NumberDisplayPad({ submitButtonText, movePage, randomPadNumber = false, action = null}: NumberDisplayPadProps) {
+
+    // 적립버튼, 사용/조회버튼, 인증버튼 구분을 어떻게 할까
+    // 리덕스를 사용해서 액션을 달리하면 되지 않을까?
+    // SUBMIT_SAVE: 적립 액션 이름
+    // SUBMIT_USE: 사용/조회 액션 이름
 
     return (
       <div className='flex h-full flex-col'>
