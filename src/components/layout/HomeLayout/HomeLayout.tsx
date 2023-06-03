91671f99e2002df719a3fec0a928d6c50b8dcbac
이동철 <choise154@gmail.com>
2023-06-03 19:55:49 +0900

    home 레이아웃 추가

---
+++ src/components/layout/HomeLayout/HomeLayout.tsx
@@ -0,0 +1,23 @@
+"use client"
+
+import { useRouter } from "next/navigation"
+
+export default function HomeLayout({ main }: { main: React.ReactNode }) {
+  const router = useRouter()
+
+  return (
+    <div className='flex flex-col h-screen'>
+      <div className='flex basis-4/5 justify-center items-center'>
+        {main}
+      </div>
+      <div className='flex flex-row basis-1/5 border-t border-t-gray'>
+        <div className='basis-1/2'>
+        </div>
+        <div className='basis-1/2 border-l border-l-gray'>
+          <button className='w-2/4 h-full border-0 border-r-0 text-2xl font-bold' onClick={() => { router.push('/use') }}>사용 / 조회</button>
+          <button className='w-2/4 h-full border-0 border-r-0 text-2xl font-bold bg-cyan-500 hover:bg-cyan-400' onClick={() => { router.push('/save') }}>적립</button>
+        </div>
+      </div>
+  </div>
+  )
+}
