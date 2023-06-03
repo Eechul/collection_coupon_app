c27b4bddd7934ecfba2e70e16c2a60c2a9927c77
이동철 <choise154@gmail.com>
2023-06-03 19:56:16 +0900

    각 페이지 숫자패드패널 컴포넌트 추가

---
+++ src/components/panel/SavePadPanel/SavePadPanel.tsx
@@ -0,0 +1,11 @@
+import NumberDisplayPad from '../../pad/NumberDisplayPad/NumberDisplayPad'
+
+export default function SavePadPanel() {
+
+    return (
+      <>
+        <NumberDisplayPad submitButtonText='적립' movePage='/certification'  />
+      </>
+    )
+  }
+  
---
+++ src/components/panel/UsePadPanel/UsePadPanel.tsx
@@ -0,0 +1,11 @@
+import NumberDisplayPad from '../../pad/NumberDisplayPad/NumberDisplayPad'
+
+export default function UsePadPanel() {
+
+    return (
+      <>
+        <NumberDisplayPad submitButtonText='사용/조회' movePage='/certification'  />
+      </>
+    )
+  }
+  
