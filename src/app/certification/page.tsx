254d82c26531cfa3dcd6ad3d049e6914190219fd
이동철 <choise154@gmail.com>
2023-06-05 23:47:38 +0900

    숫자패드 랜덤정렬기능 추가

--- src/app/certification/page.tsx
+++ src/app/certification/page.tsx
@@ -1,11 +1,9 @@
-'use client'
-
 import NumberDisplayPad from '@/components/pad/NumberDisplayPad/NumberDisplayPad'
 
-export default function Save() {
+export default function Certification() {
   return (
     <div className='w-1/2 h-full'>
-        <NumberDisplayPad submitButtonText='승인' movePage='/' />
+      <NumberDisplayPad submitButtonText='승인' movePage='/' randomPadNumber={true} />
     </div>
   )
 }
--- src/components/pad/NumberPad/NumberPad.tsx
+++ src/components/pad/NumberPad/NumberPad.tsx
@@ -1,34 +1,41 @@
 "use client"
 
-import { useRouter } from "next/navigation";
+'use client'
+
+import { useRouter } from "next/navigation"
+import { useEffect, useState } from "react"
 
 interface NumberPadProps {
   submitButtonText: string;
   movePage: string;
   randomPadNumber: boolean;
 }
-  
-export default function NumberPad({ submitButtonText, movePage, randomPadNumber }: NumberPadProps) {
 
+export default function NumberPad({ submitButtonText, movePage, randomPadNumber }: NumberPadProps) {
+  const [numbers] = useState(randomPadNumber ? [1, 2, 3, 4, 5, 6, 7, 8, 9, 0].sort(() => Math.random() - 0.5) : [1, 2, 3, 4, 5, 6, 7, 8, 9, 0])
   const router = useRouter()
 
   const test = (e: any) => {
     router.push(movePage)
   }
 
+  const handleClickedNumber = (number: Number) => {
+    console.log(number, '입니다요~')
+  }
+
   return (
     <div className='h-full flex flex-row flex-wrap'>
-      <button className='basis-1/3 text-center bg-white border border-gray text-2xl'>1</button>
-      <button className='basis-1/3 text-center bg-white border border-gray text-2xl'>2</button>
-      <button className='basis-1/3 text-center bg-white border border-gray text-2xl'>3</button>
-      <button className='basis-1/3 text-center bg-white border border-gray text-2xl'>4</button>
-      <button className='basis-1/3 text-center bg-white border border-gray text-2xl'>5</button>
-      <button className='basis-1/3 text-center bg-white border border-gray text-2xl'>6</button>
-      <button className='basis-1/3 text-center bg-white border border-gray text-2xl'>7</button>
-      <button className='basis-1/3 text-center bg-white border border-gray text-2xl'>8</button>
-      <button className='basis-1/3 text-center bg-white border border-gray text-2xl'>9</button>
+      <button className='basis-1/3 text-center bg-white border border-gray text-2xl' onClick={() => handleClickedNumber(numbers[0])}>{numbers[0]}</button>
+      <button className='basis-1/3 text-center bg-white border border-gray text-2xl' onClick={() => handleClickedNumber(numbers[1])}>{numbers[1]}</button>
+      <button className='basis-1/3 text-center bg-white border border-gray text-2xl' onClick={() => handleClickedNumber(numbers[2])}>{numbers[2]}</button>
+      <button className='basis-1/3 text-center bg-white border border-gray text-2xl' onClick={() => handleClickedNumber(numbers[3])}>{numbers[3]}</button>
+      <button className='basis-1/3 text-center bg-white border border-gray text-2xl' onClick={() => handleClickedNumber(numbers[4])}>{numbers[4]}</button>
+      <button className='basis-1/3 text-center bg-white border border-gray text-2xl' onClick={() => handleClickedNumber(numbers[5])}>{numbers[5]}</button>
+      <button className='basis-1/3 text-center bg-white border border-gray text-2xl' onClick={() => handleClickedNumber(numbers[6])}>{numbers[6]}</button>
+      <button className='basis-1/3 text-center bg-white border border-gray text-2xl' onClick={() => handleClickedNumber(numbers[7])}>{numbers[7]}</button>
+      <button className='basis-1/3 text-center bg-white border border-gray text-2xl' onClick={() => handleClickedNumber(numbers[8])}>{numbers[8]}</button>
       <button className='basis-1/3 text-center bg-white border border-gray text-2xl'>지우기</button>
-      <button className='basis-1/3 text-center bg-white border border-gray text-2xl'>0</button>
+      <button className='basis-1/3 text-center bg-white border border-gray text-2xl' onClick={() => handleClickedNumber(numbers[9])}>{numbers[9]}</button>
       <button className='basis-1/3 text-center bg-cyan-500 hover:bg-cyan-400 text-2xl' onClick={(e) => test(e)}>{submitButtonText}</button>
     </div>
   )
