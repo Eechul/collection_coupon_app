f4fc538d59a7ca554d53236b892b6f6f0edff413
이동철 <choise154@gmail.com>
2023-06-11 22:28:34 +0900

    모달을 추가해보자

--- src/app/save/layout.tsx
+++ src/app/save/layout.tsx
@@ -1,5 +1,7 @@
 import BasicLayout from '@/components/layout/BasicLayout/BasicLayout'
 
+// this is insert mode
+
 export default function SaveLayout({ children }: { children: React.ReactNode }) {
   return (
     <BasicLayout>
