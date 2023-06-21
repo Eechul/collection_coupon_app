cc45564e1d4812dc62fb19fd2adaa6fcc72a98aa
이동철 <choise154@gmail.com>
2023-06-21 23:43:02 +0900

    모달 레이아웃 개편, heroicons 아이콘 도입

--- src/components/modal/ConfirmModal/ConfirmModal.tsx
+++ src/components/modal/ConfirmModal/ConfirmModal.tsx
@@ -8,25 +8,22 @@ export default function ConfirmModal() {
   const router = useRouter()
 
   return (
-    <Modal>
-      <div className='w-full h-full bg-white '>
-        <div className='text-xl text-center border-b p-4'>사용승인</div>
-        <div className='p-6'>
-          <div className='flex'>
-            <div className='basis-1/2'>사용자</div>
-            <div className='basis-1/2'>010-5548-9173</div>
-          </div>
-          <div className='flex pt-2'>
-            <div className='basis-1/2'>사용 포인트수</div>
-            <div className='basis-1/2'>20P</div>
-          </div>
-          <div className='flex pt-2'>
-            <div className='basis-1/2'>상품항목</div>
-            <div className='basis-1/2'>아이스 아메리카노 1개</div>
-          </div>
-          <div className='flex pt-6'>
-            <button className='w-full p-2 bg-cyan-500 hover:bg-cyan-400' onClick={() => { router.push('/certification') }}>승인</button>
-          </div>
+    <Modal title="사용승인">
+      <div className='p-6'>
+        <div className='flex'>
+          <div className='basis-1/2'>사용자</div>
+          <div className='basis-1/2'>010-5548-9173</div>
+        </div>
+        <div className='flex pt-2'>
+          <div className='basis-1/2'>사용 포인트수</div>
+          <div className='basis-1/2'>20P</div>
+        </div>
+        <div className='flex pt-2'>
+          <div className='basis-1/2'>상품항목</div>
+          <div className='basis-1/2'>아이스 아메리카노 1개</div>
+        </div>
+        <div className='flex pt-6'>
+          <button className='w-full p-2 bg-cyan-500 hover:bg-cyan-400' onClick={() => { router.push('/certification') }}>승인</button>
         </div>
       </div>
     </Modal>
--- src/components/modal/Modal.tsx
+++ src/components/modal/Modal.tsx
@@ -1,49 +1,35 @@
-'use client'
+"use client"
 
-import { useCallback, useRef, useEffect } from 'react'
-import { useRouter } from 'next/navigation'
+import { useRef, useEffect } from "react"
+import { XMarkIcon } from '@heroicons/react/24/solid'
 
-export default function Modal({ children }: { children: React.ReactNode }) {
+export default function Modal({ children, title }: { children: React.ReactNode, title: string }) {
   const overlay = useRef<any>()
   const wrapper = useRef<any>()
-  const router = useRouter()
 
-  const onDismiss = useCallback(() => {
-    router.back()
-  }, [router])
-
-  const onClick = useCallback(
-    (e: any) => {
-      if (e.target === overlay.current || e.target === wrapper.current) {
-        if (onDismiss) onDismiss()
-      }
-    },
-    [onDismiss, overlay, wrapper]
-  )
-
-  const onKeyDown = useCallback(
-    (e: any) => {
-      if (e.key === "Escape") onDismiss()
-    },
-    [onDismiss]
-  );
 
   useEffect(() => {
-    document.addEventListener("keydown", onKeyDown)
-    return () => document.removeEventListener("keydown", onKeyDown)
-  }, [onKeyDown]);
+
+  }, [])
 
   return (
     <div
       ref={overlay}
-      className="fixed z-10 left-0 right-0 top-0 bottom-0 mx-auto bg-black/60"
-      onClick={onClick}
+      className='fixed z-10 left-0 right-0 top-0 bottom-0 mx-auto bg-black/60'
     >
       <div
         ref={wrapper}
-        className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-full sm:w-10/12 md:w-8/12 lg:w-1/2 p-6"
+        className='absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-full sm:w-10/12 md:w-8/12 lg:w-1/2 p-6'
       >
-        {children}
+        <div className='w-full h-full bg-white'>
+          <div className='flex  text-xl text-center border-b p-4'>
+            <div className='w-full'>{title}</div>
+            <button className='w-16'>
+              <XMarkIcon className='w-8' />
+            </button>
+          </div>
+          {children}
+        </div>
       </div>
     </div>
   );
